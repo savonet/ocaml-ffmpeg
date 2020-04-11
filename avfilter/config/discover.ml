aa93fd70a7755945e9cda2e9a6bb490e8d0f2bc2
Romain Beauxis <toots@rastageeks.org>
2020-04-11 03:23:20 -0500

    More version.

--- avfilter/config/discover.ml
+++ avfilter/config/discover.ml
@@ -11,7 +11,7 @@ let () =
           | Some pc -> (
               match
                 C.Pkg_config.query_expr_err pc ~package:"libavfilter"
-                  ~expr:"libavfilter >= 7.16.100"
+                  ~expr:"libavfilter >= 6.107.100"
               with
                 | Error msg -> failwith msg
                 | Ok deps -> deps )
